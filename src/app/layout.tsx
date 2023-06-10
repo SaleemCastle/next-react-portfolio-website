1f53a273671b9800cf27d5dba19cff49f67c7974
Saleem Castle <saleemcastle94@gmail.com>
2023-06-10 12:27:17 -0500

    seo update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { GlobalContextProvider } from '../../Context/store'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Saleem St. Michael Castle',
-  description: 'My portfolio website',
+    title: 'Saleem St. Michael Castle',
+    description: 'My react portfolio website for software engineer - Saleem Castle',
+    keyword: 'saleem, castle, saleem castle, portfolio, software engineer, react, developer, javascript, typescript, tailwind, resume'
 }
 
 export default function RootLayout({
